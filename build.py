35425172248066463325943b9c3712a388eb2566
Iddan Aaronsohn <mail@aniddan.com>
2018-11-09 00:45:58 +0200

    Build everything, concurrently

--- build.py
+++ build.py
@@ -1,48 +1,52 @@
 import sys
 import logging
 import os
+from multiprocessing.dummy import Pool
 import requests
 import docker
 from docker.errors import BuildError
 
-logging.basicConfig(level=logging.INFO)
+IMAGE = os.getenv('IMAGE')
+PACKAGE = os.getenv('PACKAGE')
 
-def get_tags(image):
-    response = requests.get(f'https://registry.hub.docker.com/v1/repositories/{image}/tags')
-    try:
-        response.raise_for_status()
-        return response.json()
-    except:
-        return []
+logging.basicConfig(level=logging.INFO)
 
 def get_versions(package):
     response = requests.get(f'https://pypi.python.org/pypi/{package}/json')
     response.raise_for_status()
     info = response.json()
-    return set(info['releases'].keys())
-
-IMAGE = os.getenv('IMAGE')
-PACKAGE = os.getenv('PACKAGE')
+    return list(info['releases'].keys())
 
 logging.info('Fetching versions...')
 versions = get_versions(PACKAGE)
-logging.info('Fetching tags...')
-tags = get_tags(IMAGE)
-deployed_versions = { tag['name'] for tag in tags }
-missing_versions = versions - deployed_versions
+latest_version = versions[-1]
 
 client = docker.from_env()
 
-for version in missing_versions:
+def push(repository, tag, **kwargs):
+   push_output_stream = client.images.push(repository, tag, stream=True, **kwargs)
+   
+   for line in push_output_stream:
+       sys.stderr.buffer.write(line + b'\n')
+
+def make_version(version):
+    logging.info(f"Building {version}...")
+
+    tag = f'{IMAGE}:{version}'
     try:
-        tag = f'{IMAGE}:{version}'
-        logging.info(f"Building {version}...")
-        print(tag)
-        client.images.build(path='.', tag=tag, buildargs={'VERSION': version})
-        logging.info(f"Pushing {version}...")
-        push_output_stream = client.images.push(IMAGE, version, stream=True)
-        for line in push_output_stream:
-            sys.stderr.buffer.write(line + b'\n')       
+        image, build_output_stream = client.images.build(path='.', tag=tag, buildargs={'VERSION': version})
 
     except BuildError as error:
         logging.error(error)
+        return
+
+    logging.info(f"Pushing {version}...")
+    push_output_stream = push(IMAGE, version)
+    
+    if version == latest_version:
+        logging.info("Pushing latest...")
+        image.tag(IMAGE, 'latest')
+        push(IMAGE, 'latest')
+            
+with Pool(processes=len(versions)) as pool:
+    pool.map(make_version, versions)
